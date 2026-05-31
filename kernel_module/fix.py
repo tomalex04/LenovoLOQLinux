87bfc58011725605e0c4e9ac6449b73aa6493b9e
tomalex04 <tomalex04@users.noreply.huggingface.co>
2026-05-31 07:31:55 +0530

    cleanup unnessecery files

--- kernel_module/fix.py
+++
@@ -1,5 +0,0 @@
-with open("legion-laptop.c", "r") as f: data = f.read()
-
-import re
-data = re.sub(r'return sysfs_emit.*?val\);', 'return sysfs_emit(buf, "%d\\n", val);', data, flags=re.DOTALL)
-with open("legion-laptop.c", "w") as f: f.write(data)
